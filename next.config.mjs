70f4dcb8c353a9105e588247d8e2d8711ceb6af5
MichelleCantu33 <analista.sistemas@biocells.med.ec>
2025-05-27 15:43:11 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Construye un archivo .dist de todo el proyecto para ser desplegado como una aplicacion estática
+    output: "export",
+};
 
 export default nextConfig;
